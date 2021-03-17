7fc629c617e4e26921d498767bac7f622d292629
Назаров Андрей Павлович <nazarov.andrey@otr.ru>
2021-03-17 14:28:18 +0000

    Update ufos_autotest.py

--- ufos_autotest.py
+++ ufos_autotest.py
@@ -22,6 +22,7 @@ log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(line
 chrome_options = Options()                                                                                                 
 chrome_options.add_argument("--ignore-certificate-errors")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--remote-debugging-port=9222")
 platform = sys.platform[0]
 if platform == 'w':
     driver = webdriver.Chrome(os.path.dirname(os.path.abspath(__file__)) + '\\chromedriver.exe', options=chrome_options)
